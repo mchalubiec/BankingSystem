aff5cb2c2a5a074045f6e7138acf377fc805b70f
Michał Chałubiec <michal.chalubiec@outlook.com>
2022-06-19 12:32:42 +0200

    added method for userinput and for press enter

---
+++ BankApp/UI/Utility.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankApp.UI
+{
+    class Utility
+    {
+        public static string GetUserInput(string prompt)
+        {
+            Console.Write($"\n\t\t{prompt,20} :  ");
+            return Console.ReadLine();
+        }
+        public static void PressEnter()
+        {
+            Console.WriteLine("\nPress any key to continue..\n");
+            Console.ReadKey();
+        }
+    }
+}
