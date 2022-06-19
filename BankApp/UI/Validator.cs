c4f8633349b2235cc837999f040ae4d447a6cd3a
Michał Chałubiec <michal.chalubiec@outlook.com>
2022-06-19 12:33:48 +0200

    added validator for variable data types

---
+++ BankApp/UI/Validator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace BankApp.UI
+{
+    class Validator
+    {
+        public static T Convert<T>(string prompt)
+        {
+            bool valid = false;
+            string userInput;
+            while (!valid)
+            {
+                userInput = Utility.GetUserInput(prompt);
+                try
+                {
+                    var converter = TypeDescriptor.GetConverter(typeof(T));
+                    if (converter != null)
+                    {
+                        return (T)converter.ConvertFromString(userInput);
+                    }
+                    else
+                    {
+                        return default;
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Invalid input");
+                }
+            }
+            return default;
+        }
+    }
+}
